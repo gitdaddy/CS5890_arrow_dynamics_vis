c944f03d8fd85f3b6f3132a388911551f89dfa46
Spencer Griffin <xcgriffs@gmail.com>
2019-12-04 21:25:58 -0700

    Added legend

--- public/Visualization/StackedBar.js
+++ public/Visualization/StackedBar.js
@@ -35,17 +35,23 @@ function updateStackedBarData(startDate, endDate) {
     //console.log(g_dataset);
     //console.log("items");
     //console.log(g_itemList);
-    stackedBarData = [];
-    _.each(g_itemList, function (d) {
-        let appliance = {};
-        appliance.name = d.filename;
-        appliance.sum = sumOfGivenItem(d.filename, startDate, endDate);
-        appliance.relativeTotal = total;
-        total += appliance.sum;
-        stackedBarData.push(appliance);
+    let theData = [];
+    _.each(getSelectedKeys(), function (d) {
+        for(let i in g_itemList){
+            if(d === g_itemList[i].filename){
+                let appliance = {};
+                appliance.name = g_itemList[i].filename;
+                appliance.sum = sumOfGivenItem(g_itemList[i].filename, startDate, endDate);
+                appliance.relativeTotal = total;
+                total += appliance.sum;
+                theData.push(appliance);
+            }
+        }
+
     });
     //console.log("stackedBarData");
     //console.log(stackedBarData);
+    stackedBarData = theData;
 }
 
 function drawStackedBar(startDate, endDate){
@@ -57,23 +63,23 @@ function drawStackedBar(startDate, endDate){
     removeStackedBar();
 
     d3.select("#stackedBarSvg")
-        .attr("width", chartWidth + chartMargin.left + chartMargin.right)
-        .attr("height", "100")
+        .attr("width", chartWidth + chartMargin.left + chartMargin.right + 20)
+        .attr("height", "40")
         .selectAll('rect')
         .data(stackedBarData)
         .enter()
         .append("g")
         .append("rect")
         .attr('x', function (d, i) {
-            return scaleBar(d.relativeTotal) + chartMargin.left + 20;
+            return scaleBar(d.relativeTotal) + chartMargin.left;
         })
         .attr('y', '10')
         .attr('width', function (d,i) {
             return scaleBar(d.sum);
         })
         .attr('height', "30")
-        .attr("fill", function (d,i) {
-            return colorSet[i];
+        .attr("fill", function (d) {
+            return chartColorScale(d.name);
         })
         .attr('value', function (d) {
             return d.name;
@@ -82,16 +88,61 @@ function drawStackedBar(startDate, endDate){
             return d.name;
         })
     ;
+
+    let boxes = d3.select("#legendSvg")
+        .attr("width", chartWidth + chartMargin.left + chartMargin.right + 20)
+        .attr("height", "100")
+        .selectAll('rect')
+        .data(getSelectedKeys())
+        .enter()
+        .append("g");
+
+        boxes.append("rect")
+        .attr('x', function (d, i) {
+            return i * 180 + chartMargin.left;
+        })
+        .attr('y', '10')
+        .attr('width', '20')
+        .attr('height', "20")
+        .attr("fill", function (d) {
+            return chartColorScale(d);
+        })
+        .attr('value', function (d) {
+            return d;
+        });
+
+    boxes.append("text")
+        .attr("x", function(d,i) {
+            console.log(i);
+            console.log(i);
+            console.log(i);
+            return i * 180 + 75;
+        })
+        .attr("y", '20')
+        .attr("dy", ".55em")
+        .text(function(d){return d})
+    ;
+
+
+
+
+
 }
 
 function removeStackedBar() {
-    console.log("REMOVEING STACKED BAR")
+    console.log("REMOVEING STACKED BAR");
     d3.select("#stackedBarSvg")
         .selectAll('g')
         .remove()
     ;
+
+    d3.select("#legendSvg")
+        .selectAll('g')
+        .remove()
+    ;
+
 }
 
 function scaleBar(value){
-    return value/total *(chartWidth + chartMargin.left + chartMargin.right);
+    return value/total *(chartWidth + chartMargin.left + chartMargin.right + 20);
 }
